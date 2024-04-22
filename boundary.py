a9393739ef90f5dc579e69925131ad3bd128099c
moyanoandres <amoyano@fi.uba.ar>
2024-04-22 04:26:28 -0300

    Menu de eleccion de query

--- boundary.py
+++ boundary.py
@@ -29,16 +29,34 @@ def config_logging(boundary_config: BoundaryConfig):
     pika_logger = logging.getLogger('pika')
     pika_logger.setLevel(logging.ERROR)
 
-def main(book_path: str, reviews_path: str):
+def main(book_path: str, reviews_path: str, query_id: str):
     boundary_config = get_config_from_env()
     config_logging(boundary_config)
 
-    child_process = multiprocessing.Process(target=input_controller.distributed_computer_books, args=(book_path,))
+    # segun la query determinar qué funcion y argumentos corresponden a su controller
+    if query_id == "1":
+        target = input_controller.distributed_computer_books
+        args = (book_path,)
+    elif query_id == "2":
+        target = input_controller.query2
+        args = ()
+    elif query_id == "3":
+        target = input_controller.query3
+        args = ()
+    elif query_id == "4":
+        target = input_controller.query4
+        args = ()
+    else:
+        target = input_controller.query5
+        args = ()
+
+    child_process = multiprocessing.Process(target=target, args=args)
     child_process.start()
     
     # Listen for results
     output_controller.display_results()
     child_process.join()
+
 
 
 if __name__ == '__main__':
@@ -50,9 +68,26 @@ if __name__ == '__main__':
     reviews_path = parser.parse_args().reviews
 
     # Determine which query to run
-    # TODO!
+    print("\n------------------------------\n")
+    print("Select a query to run: [1:5]")
+    print('1. Título, autores y editoriales de los libros de categoría "Computers"\
+           entre 2000 y 2023 que contengan "distributed" en su título.')
+    print('2. Autores con títulos publicados en al menos 10 décadas distintas')
+    print("3. Títulos y autores de libros publicados en los 90' con al menos 500 reseñas.")
+    print("4. 10 libros con mejor rating promedio entre aquellos publicados en los 90’ con al menos 500 reseñas.")
+    print('5. Títulos en categoría "Fiction" cuyo sentimiento de reseña promedio esté enel percentil 90 más alto.')
 
-    main(books_path, reviews_path)
+    query_choice = input("Enter the number of the query you want to run:")
+
+    if query_choice in ["1", "2", "3", "4", "5"]:
+        main(books_path, reviews_path, query_choice)
+    else:
+        print("Elección Incorrecta. Elija un número entre 1 y 5")
+
+    
+
+
+    
     
 
     
--- controllers/io/output_controller.py
+++ controllers/io/output_controller.py
@@ -2,10 +2,16 @@ import logging
 from messaging.goutong import Goutong
 import sys
 
+class EndOfQuery(Exception):
+    pass
+
 INPUT_QUEUE = 'results_queue'
 
 def callback_display_results(channel, method, properties, body):
     msg = body.decode()
+    if msg == "EOF":
+        logging.info("End of query")
+        raise EndOfQuery
     logging.info(msg)
 
 def display_results():
@@ -15,5 +21,8 @@ def display_results():
     messaging = Goutong()
     messaging.add_queues(INPUT_QUEUE)
     messaging.set_callback(INPUT_QUEUE, callback_display_results)
-    messaging.listen()
-
+    try:
+        messaging.listen()
+    except EndOfQuery:
+        logging.info("Stopped listening for results")
+    
