48cd95f2cd4bb37572cfbf2eb47a024ba288a178
F-Papa <francopapa21@gmail.com>
2024-05-30 20:31:02 -0300

    ControllerState generalized

---
+++ src/controller_state/controller_state.py
@@ -0,0 +1,80 @@
+import json
+import os
+
+
+class ControllerState:
+    READY_MARKER = "=-#READY#-="
+
+    def __init__(
+        self,
+        controller_id: str,
+        file_path: str,
+        temp_file_path: str,
+        extra_fields: dict,
+    ):
+        self.committed = False
+        self.controller_id = controller_id
+        self.next_transaction = 1
+        self.transactions_received = []
+        self.data = []
+        self.file_path = file_path
+        self.temp_file_path = temp_file_path
+        self.extra_fields = set(extra_fields.keys())
+
+        for key in extra_fields:
+            setattr(self, key, extra_fields[key])
+
+    def save_to_disk(self):
+        to_save = {
+            "next_transaction": self.next_transaction,
+            "transactions_received": self.transactions_received,
+            "committed": self.committed,
+            "data": self.data,
+        }
+
+        for key in self.extra_fields:
+            to_save[key] = getattr(self, key)
+
+        with open(self.temp_file_path, "w") as f:
+            f.write(json.dumps(to_save) + "\n")
+            f.write(self.READY_MARKER)
+
+        os.replace(self.temp_file_path, self.file_path)
+    
+    def get(self, key: str):
+        return getattr(self, key)
+    
+    def set(self, key: str, value):
+        if not key in self.__dict__: 
+            raise Exception(f"{key} was not declared in the constructor")
+        setattr(self, key, value)
+
+    def update_from_file(self, file_path: str):
+        with open(file_path, "r") as f:
+            file_lines = f.readlines()
+
+        if not self._is_file_valid(file_lines):
+            raise Exception("Invalid State File")
+
+        state_in_file = json.loads(file_lines[0])
+
+        for key in state_in_file:
+            setattr(self, key, state_in_file[key])
+
+    def _is_file_valid(self, lines: list):
+        return len(lines) == 2 and lines[-1] == self.READY_MARKER
+
+
+    def is_transaction_received(self, transaction_id: str) -> bool:
+        return transaction_id in self.transactions_received
+
+    def id_for_next_transaction(self) -> str:
+        return f"{self.controller_id}#{self.next_transaction}"
+
+    def mark_transaction_received(self, transaction_id: str):
+        self.transactions_received.append(transaction_id)
+
+    def mark_transaction_committed(self):
+        self.committed = True
+        self.next_transaction += 1
+        self.save_to_disk()
--- src/controllers/filters/title_filter/main.py
+++ src/controllers/filters/title_filter/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from src.controller_state.controller_state import ControllerState
 from src.messaging.goutong import Goutong
 from src.exceptions.shutting_down import ShuttingDown
 import logging
@@ -19,144 +20,12 @@ OUTPUT_Q1 = "category_filter_queue"
 shutting_down = False
 
 
-class FilterState:
-    READY_MARKER = "=-#READY#-="
-
-    def __init__(
-        self,
-        file_path: str,
-        committed: bool,
-        transactions_received: list[str],
-        next_transaction: int,
-        data: list,
-        transaction_prefix: str,
-        conn_id: int,
-        queries: list[int],
-        eof: bool,
-    ):
-        self.conn_id = conn_id
-        self.queries = queries
-        self.eof = eof
-        self.file_path = file_path
-        self.committed = committed
-        self.transactions_received = transactions_received
-        self.next_transaction = next_transaction
-        self.data = data
-        self.transaction_prefix = transaction_prefix
-
-    @classmethod
-    def create_default(cls, file_path: str, transaction_prefix: str):
-        return FilterState(
-            file_path=file_path,
-            committed=True,
-            transactions_received=[],
-            next_transaction=1,
-            data=[],
-            transaction_prefix=transaction_prefix,
-            conn_id=0,
-            queries=[],
-            eof=False,
-        )
-
-    @classmethod
-    def load_or_init(cls, file_path: str, transaction_prefix: str):
-
-        if not os.path.exists(file_path):
-            logging.warning("State File Not Found. Creating New State.")
-            return cls.create_default(file_path, transaction_prefix)
-
-        loaded_state = dict()
-
-        with open(file_path, "r") as f:
-            lines = f.readlines()
-
-        is_valid = len(lines) == 2 and lines[-1] == cls.READY_MARKER
-
-        if not is_valid:
-            raise Exception("Invalid State File")
-
-        loaded_state = json.loads(lines[0])
-
-        return cls(
-            file_path,
-            loaded_state["committed"],
-            loaded_state["transactions_received"],
-            loaded_state["next_transaction"],
-            loaded_state["data"],
-            transaction_prefix,
-            loaded_state["conn_id"],
-            loaded_state["queries"],
-            loaded_state["eof"],
-        )
-
-    def is_transaction_received(self, transaction_id: str) -> bool:
-        return transaction_id in self.transactions_received
-
-    def id_for_next_transaction(self) -> str:
-        return f"{self.transaction_prefix}{self.next_transaction}"
-
-    def mark_transaction_received(self, transaction_id: str):
-        self.transactions_received.append(transaction_id)
-        self.save_to_disk()
-
-    def new_data(
-        self,
-        data: list,
-        transaction_id: str,
-        queries: list[int],
-        conn_id: int,
-        eof: bool = False,
-    ):
-        self.data = data
-        self.committed = False
-        self.transactions_received.append(transaction_id)
-        self.queries = queries
-        self.conn_id = conn_id
-        self.eof = eof
-        self.save_to_disk()
-
-    def mark_transaction_committed(self):
-        self.committed = True
-        self.next_transaction += 1
-        self.save_to_disk()
-
-    def save_to_disk(self):
-        tmp_file = self.file_path + ".tmp"
-
-        to_save = {
-            "committed": self.committed,
-            "transactions_received": self.transactions_received,
-            "next_transaction": self.next_transaction,
-            "data": self.data,
-            "conn_id": self.conn_id,
-            "queries": self.queries,
-            "eof": self.eof,
-        }
-
-        # to_show = to_save.copy()
-        # to_show["data"] = f'{len(to_show["data"])} items' if to_show["data"] else "None"
-        # to_show["transactions_received"] = f'{len(to_show["transactions_received"])} items' if to_show["transactions_received"] else "None"
-
-        # if None in to_save["transactions_received"]: 
-        #     to_show["transactions_received"] += " + None"
-
-        # logging.info(f"Saving State to Disk: {to_show}")
-
-        with open(tmp_file, "w") as f:
-            f.write(json.dumps(to_save) + "\n")
-            f.write(self.READY_MARKER)
-
-        os.replace(tmp_file, self.file_path)
-
-
 # Graceful Shutdown
 def sigterm_handler():
     global shutting_down
     logging.info("SIGTERM received. Initiating Graceful Shutdown.")
     shutting_down = True
     raise ShuttingDown
-    # msg = Message({"ShutDown": True})
-    # messaging.broadcast_to_group(CONTROL_GROUP, msg)
 
 
 def config_logging(level: str):
@@ -190,8 +59,24 @@ def main():
     logging.info(filter_config)
 
     # Load State
-    filter_id = f"{FILTER_TYPE}_{filter_config.get('FILTER_NUMBER')}"
-    state = FilterState.load_or_init("state/state_file", filter_id)
+    controller_id = f"{FILTER_TYPE}_{filter_config.get('FILTER_NUMBER')}"
+
+    extra_fields = {
+        "books_received": [],
+        "conn_id": 0,
+        "queries": [],
+        "EOF": False,
+    }
+
+    state = ControllerState(
+        controller_id=controller_id,
+        file_path=f"state/{controller_id}.json",
+        temp_file_path=f"state/{controller_id}.tmp",
+        extra_fields=extra_fields,
+    )
+
+    if os.path.exists(state.file_path):
+        state.update_from_file(state.file_path)
 
     # Set up queues
     control_queue_name = (
@@ -220,48 +105,63 @@ def main():
         state.save_to_disk()
 
 
-def handle_uncommited_transactions(messaging: Goutong, state: FilterState):
-    if state.data:
-        to_send = filter_data(state.data)
-
-        _send_batch_q1(messaging, to_send, state.conn_id, state.queries)
-    if state.eof:
-        _send_EOF(messaging, state.conn_id)
+def handle_uncommited_transactions(messaging: Goutong, state: ControllerState):
+    if state.get("books_received"):
+        to_send = filter_data(state.get("books_received"))
+        _send_batch_q1(
+            messaging=messaging,
+            batch=to_send,
+            conn_id=state.get("conn_id"),
+            queries=state.get("queries"),
+        )
+    if state.get("EOF"):
+        _send_EOF(messaging=messaging, conn_id=state.get("conn_id"))
     state.mark_transaction_committed()
 
 
-def main_loop(messaging: Goutong, input_queue_name: str, state: FilterState):
+def main_loop(messaging: Goutong, input_queue_name: str, state: ControllerState):
     messaging.set_callback(
-        input_queue_name, receive_new_data, auto_ack=False, args=(state,)
+        input_queue_name, callback_title_filter, auto_ack=False, args=(state,)
     )
     messaging.listen()
-    if state.data:
-        to_send = filter_data(state.data)
-        _send_batch_q1(messaging, to_send, state.conn_id, state.queries)
-    if state.eof:
-        _send_EOF(messaging, state.conn_id)
+    if state.get("books_received"):
+        to_send = filter_data(state.get("books_received"))
+        _send_batch_q1(
+            messaging=messaging,
+            batch=to_send,
+            conn_id=state.get("conn_id"),
+            queries=state.get("queries"),
+        )
+
+    if state.get("EOF"):
+        _send_EOF(messaging, state.get("conn_id"))
 
     state.mark_transaction_committed()
 
 
-def receive_new_data(messaging: Goutong, msg: Message, state: FilterState):
+def callback_title_filter(messaging: Goutong, msg: Message, state: ControllerState):
+    transaction_id = msg.get("transaction_id")
 
-    # Any book has a title with the keyword "distributed"
-    if msg.get("transaction_id") in state.transactions_received:
+    # Ignore duplicate transactions
+    if transaction_id in state.transactions_received:
         messaging.ack_delivery(msg.delivery_id)
         logging.info(f"Received Duplicate Transaction {msg.get('transaction_id')}")
         return
 
+    # Add new data to state
     eof = msg.has_key("EOF")
+    books_received = msg.get("data") if msg.has_key("data") else []
+    conn_id = msg.get("conn_id")
+    transaction_id = msg.get("transaction_id")
 
-    state.new_data(
-        data=msg.get("data"),
-        transaction_id=msg.get("transaction_id"),
-        queries=msg.get("queries"),
-        conn_id=msg.get("conn_id"),
-        eof=eof,
-    )
+    state.set("books_received", books_received)
+    state.set("conn_id", conn_id)
+    state.set("queries", msg.get("queries"))
+    state.set("EOF", eof)
+    state.mark_transaction_received(transaction_id)
+    state.save_to_disk()
 
+    # Acknowledge message now that it's saved
     messaging.ack_delivery(msg.delivery_id)
     messaging.stop_consuming(msg.queue_name)
 
@@ -277,13 +177,6 @@ def filter_data(data: list):
     if to_return:
         logging.info(f"Filtered {len(to_return)} items")
     return to_return
-
-
-# def callback_control(messaging: Goutong, msg: Message):
-#     global shutting_down
-#     if msg.has_key("ShutDown"):
-#         shutting_down = True
-#         raise ShuttingDown
 
 
 def _columns_for_query1(book: dict) -> dict:
@@ -310,35 +203,6 @@ def _send_EOF(messaging: Goutong, conn_id: int):
     )
     messaging.send_to_queue(EOF_QUEUE, msg)
     logging.debug(f"Sent EOF to: {EOF_QUEUE}")
-
-
-# def callback_filter(messaging: Goutong, msg: Message, config: Configuration):
-#     # logging.debug(f"Received: {msg.marshal()}")
-
-#     queries = msg.get("queries")
-#     connection_id = msg.get("conn_id")
-
-#     if msg.has_key("EOF"):
-#         # Forward EOF and Keep Consuming
-#         _send_EOF(messaging, connection_id)
-#         return
-
-#     books = msg.get("data")
-#     batch_q1 = []
-
-#     for book in books:
-#         title = book.get("title")
-
-#         # Query 1 (Only) Flow
-#         if KEYWORD_Q1.lower() in title.lower():
-#             batch_q1.append(book)
-#             if len(batch_q1) >= config.get("ITEMS_PER_BATCH"):
-#                 _send_batch_q1(messaging, batch_q1, connection_id)
-#                 batch_q1.clear()
-
-#     # Send remaining items
-#     if batch_q1:
-#         _send_batch_q1(messaging, batch_q1, connection_id)
 
 
 if __name__ == "__main__":
